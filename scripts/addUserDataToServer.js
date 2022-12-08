2923736594ada043c4930a73b12e3604384e9a3e
Jadyn-Jenkins <Noblejadynjenkins@gmail.com>
2022-12-08 13:47:33 -0500

    Create pullData and postData fucntion. Create DOM element variables.

---
+++ scripts/addUserDataToServer.js
@@ -0,0 +1,24 @@
+const userNameDropDownVal = document.querySelector('#userName').value;
+const categoryDropDownVal = document.querySelector('#category').value;
+const priorityDropDownVal = document.querySelector('#priority').value;
+const deadlineDateVal = document.querySelector('#deadline').value;
+const descriptionTxtAreaVal = document.querySelector('#description').value;
+//button const
+
+/* 
+{
+    "userid":5,
+    "category":"Personal Task",
+    "description":"Finish studying for ENG 211 exam",
+    "deadline":"2022-12-15",
+    "priority":"Medium",
+}
+ */
+
+function pullData() {
+    
+}
+
+function postData() {
+    
+}
