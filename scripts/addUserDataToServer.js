4f84717a3ad338af84ba6f71424fb40dd4a75e42
Charonn <charonn.edwards412@gmail.com>
2022-12-09 11:50:04 -0500

    add functionality to newTodo.html using addUserDataToServer

--- scripts/addUserDataToServer.js
+++ scripts/addUserDataToServer.js
@@ -1,24 +1,42 @@
-const userNameDropDownVal = document.querySelector('#userName').value;
-const categoryDropDownVal = document.querySelector('#category').value;
-const priorityDropDownVal = document.querySelector('#priority').value;
-const deadlineDateVal = document.querySelector('#deadline').value;
-const descriptionTxtAreaVal = document.querySelector('#description').value;
-//button const
+// const userNameDropDownVal = document.querySelector('#userName').value;
+// const categoryDropDownVal = document.querySelector('#category').value;
+// const priorityDropDownVal = document.querySelector('#priority').value;
+// const deadlineDateVal = document.querySelector('#deadline').value;
+// const descriptionTxtAreaVal = document.querySelector('#description').value;
+const btn = document.getElementById("inputBtn");
+const formEl = document.getElementById("inputData");
 
-/* 
-{
-    "userid":5,
-    "category":"Personal Task",
-    "description":"Finish studying for ENG 211 exam",
-    "deadline":"2022-12-15",
-    "priority":"Medium",
-}
- */
+formEl.addEventListener("submit", getData);
 
-function pullData() {
-    
+
+let formJSON;
+
+function getData(evt) {
+  console.log(`button works ${btn}`);
+  evt.preventDefault();
+  const data = new FormData(evt.target);
+  console.log(data.entries);
+  formJSON = Object.fromEntries(data.entries());
+  console.log(formJSON);
+  sendData();
 }
 
-function postData() {
-    
+function sendData() {
+  fetch("http://localhost:8083/api/todos/", {
+    method: "POST",
+    body: JSON.stringify(formJSON),
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      let confirmationMessage = document.getElementById("confirmationMessage");
+      confirmationMessage.innerHTML = "New Todo Added";
+    })
+    .catch((err) => {
+      console.log(err);
+      let confirmationMessage = document.getElementById("confirmationMessage");
+      confirmationMessage.innerHTML = "Todo has NOT been added, try again";
+    });
 }
+
+
--- scripts/populateDropDown.js
+++ scripts/populateDropDown.js
@@ -1,44 +1,44 @@
-let newUsernameEl = document.getElementById('userid');
-let categoryEl = document.getElementById('category');
-let usernameEl = document.getElementById('todoDropdown');
+let newUsernameEl = document.getElementById("userid");
+let categoryEl = document.getElementById("category");
+let usernameEl = document.getElementById("todoDropDown");
 
 
 
-        fetch('http://localhost:8083/api/users')
-.then(response => response.json())
-.then(data => {
+
+fetch("http://localhost:8083/api/users")
+  .then((response) => response.json())
+  .then((data) => {
     for (let i = 0; i < data.length; i++) {
-        let opt = document.createElement("option");
-        let text = opt.textContent = data[i].name
-        let value = opt.value = data[i].id;
-        console.log(value)
-        newUsernameEl.appendChild(opt);
+      let opt = document.createElement("option");
+      let text = (opt.textContent = data[i].name);
+      let value = (opt.value = data[i].id);
+      console.log(value);
+      newUsernameEl.appendChild(opt);
     }
+  });
+
+  fetch("http://localhost:8083/api/users")
+  .then((response) => response.json())
+  .then((data) => {
     for (let i = 0; i < data.length; i++) {
-        let option = document.createElement("option");
-        let text = option.textContent = data[i].username
-        option.value = data[i].username;
-        usernameEl.appendChild(option);
-   }
-});
-
-fetch('http://localhost:8083/api/categories')
-.then(response => response.json())
-.then(data => {
-for (let i = 0; i < data.length; i++) {
-     let opt = document.createElement("option");
-     let text = opt.textContent = data[i].name
-     opt.value = data[i].name;
-     categoryEl.appendChild(opt);
- }
-});
+      let opt = document.createElement("option");
+      let text = (opt.textContent = data[i].username);
+      opt.name = data[i].name 
+      opt.id = data[i].name
+      opt.value = data[i].username;
+      usernameEl.appendChild(opt);
+    }
+  });
 
 
 
-
-
-
-
-
-     
-
+fetch("http://localhost:8083/api/categories")
+  .then((response) => response.json())
+  .then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      let opt = document.createElement("option");
+      let text = (opt.textContent = data[i].name);
+      opt.value = data[i].name;
+      categoryEl.appendChild(opt);
+    }
+  });
